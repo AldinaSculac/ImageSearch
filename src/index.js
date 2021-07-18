7367cefd49e7fa7635de06609351f3f5d855c5ee
unknown <sculac.aldina@gmail.com>
2021-07-18 16:50:42 +0200

    Custom Theme for Chakra UI Link

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme, colorScheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  components: {
+    Link: {
+      variants: {
+        // you can name it whatever you want
+        primary: ({ colorScheme = "whiteAlpha" }) => ({
+          color: `${colorScheme}.800`,
+          fontWeight:'normal',
+          textTransform:'uppercase',
+          _hover: {
+            color: `${colorScheme}.600`,
+            textDecoration:'none',
+          },
+        }),
+      },
+      defaultProps: {
+        // you can name it whatever you want
+        variant: "primary",
+      },
+    },
+  },
+})
+
 
 ReactDOM.render(
-  <ChakraProvider >
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>,
   document.getElementById('root')
