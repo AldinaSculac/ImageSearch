82e42f0b800726d3f232dc5ea99cfa2471432aa7
unknown <sculac.aldina@gmail.com>
2021-07-27 19:32:57 +0200

    container for home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Search from '../pics/Search';
+import {
+  Container,
+  VStack
+} from "@chakra-ui/react"
 
 const Home = () => {
   return (
-    <>
-      <h1>Home page</h1>
-    </>
+    <VStack>
+      <Container maxW="container.md" m={6} >
+        <Search />
+      </Container>
+    </VStack>
   )
 }
 
