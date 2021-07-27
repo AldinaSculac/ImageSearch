ba453eba7934718219058f5e36d70683f629e5a4
unknown <sculac.aldina@gmail.com>
2021-07-27 19:11:31 +0200

    clear_pics reducer

--- src/context/pic/picReducer.js
+++ src/context/pic/picReducer.js
@@ -10,10 +10,16 @@ export default (state, action) => {
   switch (action.type) {
     case SEARCH_PICS:
       return {
-        ...state, 
-        pics: action.payload, 
+        ...state,
+        pics: action.payload,
         loading: false
       };
+    case CLEAR_PICS:
+      return {
+        ...state,
+        pics: [],
+        loading: false
+      }
     default:
       return state;
   }
