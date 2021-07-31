7af2eb49ac108d76a73552b8ce496358b5ed24a1
unknown <sculac.aldina@gmail.com>
2021-07-31 19:52:58 +0200

    New UI added - Material UI - pt2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Grid, Paper} from '@material-ui/core';
+import {ThemeProvider} from '@material-ui/core/styles';
+import theme from './theme';
+
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
-
 import PicState from './context/pic/PicState';
+
 
 const App = () => {
   return (
     <PicState>
       <Router>
-        <div>
-          <Navbar title="Pics App" />
-          <div>
-            <Switch>
-              <Route exact path="/" render={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </div>
+        <ThemeProvider theme={theme} >
+        <Paper elevation={0} square={true} style={{minHeight: '100vh'}}>
+          <Grid container direction="column">
+            <Grid item>
+              <Navbar title="Pics App" />
+            </Grid>
+            <Grid item container>
+              <Switch>
+                <Route exact path="/" render={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </Grid>
+          </Grid>
+        </Paper>
+      </ThemeProvider>
       </Router>
     </PicState>
-    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,42 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react';
 
-const config = {
-  initialColorMode: 'dark',
-  useSystemColorMode: false
-}
-
-const theme = extendTheme({
-  config,
-  components: {
-    Link: {
-      variants: {
-        // you can name it whatever you want
-        primary: ({ colorScheme = "whiteAlpha" }) => ({
-          color: `${colorScheme}.800`,
-          fontWeight:'normal',
-          textTransform:'uppercase',
-          _hover: {
-            color: `${colorScheme}.600`,
-            textDecoration:'none',
-          },
-        }),
-      },
-      defaultProps: {
-        // you can name it whatever you want
-        variant: "primary",
-      },
-    },
-  },
-})
-
-
-ReactDOM.render(
-  <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
-  </ChakraProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
