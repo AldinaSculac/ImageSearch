86d33ae0be7471ca7dc65d468d77335fe053ce2d
unknown <sculac.aldina@gmail.com>
2021-08-01 17:04:59 +0200

    Alert Context

--- src/App.js
+++ src/App.js
@@ -6,28 +6,32 @@ import theme from './theme';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+
 import PicState from './context/pic/PicState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
   return (
     <PicState>
-      <Router>
-        <ThemeProvider theme={theme} >
-        <Paper elevation={0} square={true} style={{minHeight: '100vh'}}>
-          <Grid container direction="column">
-            <Grid item>
-              <Navbar title="Pics App" />
-            </Grid>
-            <Grid item container>
-              <Switch>
-                <Route exact path="/" render={Home} />
-                <Route exact path="/about" component={About} />
-              </Switch>
-            </Grid>
-          </Grid>
-        </Paper>
-      </ThemeProvider>
-      </Router>
+      <AlertState>
+        <Router>
+          <ThemeProvider theme={theme} >
+            <Paper elevation={0} square={true} style={{minHeight: '100vh'}}>
+              <Grid container direction="column">
+                <Grid item>
+                  <Navbar title="Pics App" />
+                </Grid>
+                <Grid item container>
+                  <Switch>
+                    <Route exact path="/" render={Home} />
+                    <Route exact path="/about" component={About} />
+                  </Switch>
+                </Grid>
+              </Grid>
+            </Paper>
+        </ThemeProvider>
+        </Router>
+      </AlertState>
     </PicState>
   );
 }
