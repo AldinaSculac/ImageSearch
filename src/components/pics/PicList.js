4b24482eafa5e23f6ea5331f5ec48f393e78087f
unknown <sculac.aldina@gmail.com>
2021-08-01 17:22:31 +0200

    Cleanup

--- src/components/pics/PicList.js
+++ src/components/pics/PicList.js
@@ -1,27 +1,30 @@
 import React, { useContext } from 'react';
 import PicContext from '../../context/pic/picContext';
+import AlertContext from '../../context/pic/picContext';
 import {Grid, CircularProgress } from '@material-ui/core';
 import PicItem from './PicItem';
 
 const PicList = () => {
   const {pics, loading} = useContext(PicContext);
+  const {setAlert} = useContext(AlertContext);
 
   if (loading) {
-    return (<div style={{ padding: '10%' }}>
-              <Grid container 
-                  alignItems="center" 
-                  justifyContent="center"
-                >
-                <CircularProgress color="primary" />
-              </Grid>
-            </div>);
+    return (
+      <div style={{ padding: '10%' }}>
+        <Grid container 
+            alignItems="center" 
+            justifyContent="center"
+          >
+          <CircularProgress color="primary" />
+        </Grid>
+      </div>);
   } else {
     return (
-        <Grid container>
-          {pics.map(pic => (
+      <Grid container>
+        {pics.map(pic => (
           <PicItem key={pic.id} pic={pic} />
         ))}
-        </Grid>
+      </Grid>
     );
   }
 }
